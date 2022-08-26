6150c6d6b3f8ecb97fb0e756438af867426f6cea
Frank <frank_davenport@ucsb.edu>
2022-08-26 12:23:56 -0700

    finished commenting 01

--- 01_forecast_tethys_static_files.R
+++ 01_forecast_tethys_static_files.R
@@ -35,8 +35,8 @@ source('999_forecast_tethys_custom_functions.R')
 
 
 ##-- SET UP PARAMETERS FOR SEASON DEKAD AND Product
-SEASON<-'L'  #what historical dekads- including all 3 breaks shapefile colunm limits
-PRODUCT<-'Maize'     #'Maize'  #Sorghum
+SEASON<-'L'  #'S'  Pick a Season 
+PRODUCT<-'Maize'     #'Maize'  #'Sorghum'  Pick a Crop
 
 # Ingest Main Formatted Viewer File ---------------------------------------------------------
 setwd(dirRdata)
@@ -59,6 +59,8 @@ static_vars_mean_last10<-paste0(static_vars_mean,'_last10')
 
 static_vars_all<-c(static_vars_obs,static_vars_mean,static_vars_mean_all,static_vars_mean_last10)
 
+static_vars_all  #Have a look at the varialbes
+
 # Create Annual Ag Stats Shapes -------------------------------------------
 
 #--Select out Ag Stats
@@ -68,14 +70,16 @@ dt<-droplevels(dt)
 
 dt<-dplyr::select(dt,fnid:season,var_alias,value)
 
+#--Create a dataframe with just the mean values
 dtm<-filter(dt,is.na(year)) # for mean values take out values that do not have years
 
 #--Values by Year
-dt<-filter(dt,!is.na(year))
+dt<-filter(dt,!is.na(year))  #
 head(dt)
 
 dtw<-pivot_wider(dt,names_from=c(var_alias,year),values_from=value,names_sep="")  #MAKE A "WIDE" version to be a shapefile
 
+#Examine the file-- there should be 1 row for each admin unit
 head(dtw)
 
 ##-If above command gives duplicates Warning do the following:
@@ -91,27 +95,36 @@ dtmw<-pivot_wider(dtm,names_from=c(var_alias),values_from=value,names_sep="")
 
 #--Check that Names are not more than 10 Characters Long
 #--This function is in 999_forecast_tethys_custom_functions.R
+#--It will print out the names of any colunms more than 10 characters long
 fun.nl(dtw)
 fun.nl(dtmw)
 
 
 #--Use fun.join to create seperate lists for each main variable
 #--This function is in 999_forecast_tethys_custom_functions.R
+#--The resulting list will have 2 data frames- one named 'obs' for observed values and one names 'means' for mean values
 
 ### $obs has observations, $means has means
 dsa<-fun.join(dtmw,dtw,var='a') #area stats
 dsp<-fun.join(dtmw,dtw,var='p') #production stats
 dsy<-fun.join(dtmw,dtw,var='y') #yield stats
 
+#--Explore These
+names(dsa)
+head(dsa$means)
+head(dsa$obs)
+
 # Read in a Shapefile and Join with Shape ---------------------------------
 setwd(dirViewer)
-dshp<-st_read('viewer_data.shp')
-dshp<-dplyr::select(dshp,FNID,ADMIN0)
+dshp<-st_read('viewer_data.shp')  #shapefile with the all of the admin units we forecast for
+dshp<-dplyr::select(dshp,FNID,ADMIN0) #we will join on 'fnid'
 #dshp$ADMIN2<-dshp$ADMIN1
 
 
 #--Create Shapefiles
 #--Observed Area, Production, and Yields 
+#-- fun.spjoin() is standard 'inner_join'- but also prints the number of colunms and x2 checks in any colunm names are 2 long
+#-- this is importance- shapefiles (usually) cannot have more than 256 colunms.  If colunm names are 2 long, they could get cut off and the viewer will not map them correctly.
 sda<-fun.spjoin(dshp,dsa$obs)
 sdp<-fun.spjoin(dshp,dsp$obs)
 sdy<-fun.spjoin(dshp,dsy$obs)
@@ -122,19 +135,25 @@ sdpm<-fun.spjoin(dshp,dsp$means)
 sdym<-fun.spjoin(dshp,dsy$means)
 
 # Write Out ---------------------------------------------------------------
+##- These if() statements will make sure that the shapefile goes in the correct directory
 if(SEASON=='L'){DIR_sub<-'S1/'} #Main or long rains season
 if(SEASON=='S'){DIR_sub<-'S2/'} # Secondary or Short rains season
 
-#-Different Sub Directories if Product not Maize
+#-Make sure this goes in the correct product sub-directory
 DIR_sub<-paste0(DIR_sub,PRODUCT,'/')
 
 #-Write out to Static Directories
 setwd(paste0(dirViewerOutStatic,DIR_sub))
 
-st_write(sda,paste0(SEASON,'_area.shp'),delete_dsn=TRUE)
+#delete_dsn will delete the existing shapefile- if the shapefile does not exist, you will get a warning, but it will still write it out
+st_write(sda,paste0(SEASON,'_area.shp'),delete_dsn=TRUE)  
 st_write(sdp,paste0(SEASON,'_prod.shp'),delete_dsn=TRUE)
 st_write(sdy,paste0(SEASON,'_yield.shp'),delete_dsn=TRUE)
 
 st_write(sdam,paste0(SEASON,'_area_mn.shp'),delete_dsn=TRUE)
 st_write(sdpm,paste0(SEASON,'_prod_mn.shp'),delete_dsn=TRUE)
 st_write(sdym,paste0(SEASON,'_yield_mn.shp'),delete_dsn=TRUE)
+
+#----Explore the shapefiles
+head(sda)
+summary(sda)
